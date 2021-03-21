dde5a79b1585ea99462ff757eb18cb6d11ee6c55
Lau <laststandrighthere@gmail.com>
2021-03-21 22:43:09 +0530

    msm: ipa: Add a option to allow IPA wakelocks
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -3364,6 +3364,7 @@ void ipa2_dec_client_disable_clks(struct ipa_active_client_logging_info *id)
 }
 
 #ifndef CONFIG_DISABLE_IPA_WAKELOCKS
+#ifdef CONFIG_IPA_WAKELOCK
 /**
  * ipa_inc_acquire_wakelock() - Increase active clients counter, and
  * acquire wakelock if necessary
@@ -3407,6 +3408,9 @@ void ipa_dec_release_wakelock(enum ipa_wakelock_ref_client ref_client)
 		ipa_ctx->wakelock_ref_cnt.cnt, ref_client);
 	spin_unlock_irqrestore(&ipa_ctx->wakelock_ref_cnt.spinlock, flags);
 }
+#else
+void ipa_inc_acquire_wakelock(enum ipa_wakelock_ref_client ref_client) {}
+void ipa_dec_release_wakelock(enum ipa_wakelock_ref_client ref_client) {}
 #endif
 
 static int ipa_setup_bam_cfg(const struct ipa_plat_drv_res *res)
