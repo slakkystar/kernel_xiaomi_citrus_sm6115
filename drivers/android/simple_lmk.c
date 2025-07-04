962aa6896dcc7b5e606f5ce388a63323a173c706
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-07-04 00:52:14 +0000

    simple_lmk: Configure dynamic minfree for our devices
    - Change dynamic minfree values for 4GB and 6GB RAM variant.
    - Dynamic minfree values taken from [1].
    
    [1] - https://github.com/alternoegraha/kernel_xiaomi_sm6225/commit/09c40161533a96dbea1c82131f2284337fb141e0
    Co-authored-by: alternoegraha <noegrahachan@gmail.com>
    Change-Id: Iccfb8eea9aa279a1e7e586638a77a32c04cac06a
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -497,13 +497,13 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 
 	si_meminfo(&i);
 	if (i.totalram << (PAGE_SHIFT-10) > 4096ull * 1024) {
-	  // from - phone-xhdpi-6144-dalvik-heap.mk
-	  slmk_minfree = 125;
-	  slmk_timeout = 160;
+	  // 6GB variant
+	  slmk_minfree = 128;
+	  slmk_timeout = 150;
 	} else {
-	  // from - phone-xhdpi-4096-dalvik-heap.mk
-	  slmk_minfree = 155;
-	  slmk_timeout = 145;
+	  // 4GB variant
+	  slmk_minfree = 146;
+	  slmk_timeout = 100;
 	}
 
 	return 0;
