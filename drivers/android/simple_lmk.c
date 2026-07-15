291d3e5de9a2ca93507ec39c1d6ddb0d70360d66
anggitmrt87 <jarbull87@gmail.com>
2026-07-15 03:07:26 +0000

    Revert "simple_lmk: convert si_meminfo into totalram_pages"
    This reverts commit 0bd85c4da1cda035318909a45a326eb4ae1c178c.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -492,7 +492,7 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 {
 	static atomic_t init_done = ATOMIC_INIT(0);
 	struct task_struct *thread;
-	unsigned long total_mb = 0;
+	struct sysinfo i;
 
 	if (!atomic_cmpxchg(&init_done, 0, 1)) {
 		thread = kthread_run(simple_lmk_reaper_thread, NULL,
@@ -504,19 +504,17 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
 	}
 
-	total_mb = totalram_pages >> (20 - PAGE_SHIFT);
-	if (total_mb > 3072) {
-		// 4GB+ variant
-		slmk_minfree = 155;
-		slmk_timeout = 145;
+	si_meminfo(&i);
+	if (i.totalram << (PAGE_SHIFT-10) > 4096ull * 1024) {
+	  // 6GB variant
+	  slmk_minfree = 128;
+	  slmk_timeout = 150;
 	} else {
-		// 3GB or lower
-		slmk_minfree = 256;
-		slmk_timeout = 200;
+	  // 4GB variant
+	  slmk_minfree = 146;
+	  slmk_timeout = 100;
 	}
 
-	pr_info_once("Detected %lu megabytes of device RAM, setting minfree to %hu with timeout of %hu\n",
-		total_mb, slmk_minfree, slmk_timeout);
 	return 0;
 }
 
