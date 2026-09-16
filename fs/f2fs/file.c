087816569e58ba2cea736231fc5b2f87fd0706c5
SlakkyStar <slakkystar@gmail.com>
2026-09-16 21:46:30 +0500

    mm: vmscan: tune reclaim swappiness for the hybridswapd thread
    Signed-off-by: SlakkyStar <slakkystar@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -4588,7 +4588,15 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	}
 	/* Possibly preallocate the blocks for the write. */
 	target_size = iocb->ki_pos + iov_iter_count(from);
-	preallocated = f2fs_preallocate_blocks(iocb, from);
+
+#ifdef CONFIG_HYBRIDSWAP_CORE
+	if ((iocb->ki_flags & IOCB_DIRECT) &&
+	    f2fs_overwrite_io(inode, iocb->ki_pos, iov_iter_count(from)))
+		preallocated = 0;
+	else
+#endif
+		preallocated = f2fs_preallocate_blocks(iocb, from);
+
 	if (preallocated < 0) {
 		ret = preallocated;
 	} else {
