6691cb5232ea2c39fb46983cedd91bc58e1bf491
SlakkyStar <slakkystar@gmail.com>
2026-09-17 12:36:58 +0500

    Revert "fs, kernel, input: Implement scope-minimized manual hooks v2.0"
    This reverts commit a96c099c9236fb1f8ecbad298fac11890a53d16b.

--- fs/exec.c
+++ fs/exec.c
@@ -1751,16 +1751,6 @@ static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 /*
  * sys_execve() executes a new program.
  */
-#ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern bool ksu_su_compat_enabled __read_mostly;
-extern bool __ksu_is_allow_uid_for_current(uid_t uid);
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
-				void *envp, int *flags);
-#endif
-
 static int __do_execve_file(int fd, struct filename *filename,
 			    struct user_arg_ptr argv,
 			    struct user_arg_ptr envp,
@@ -1773,19 +1763,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
-#ifdef CONFIG_KSU
-	if (!ksu_su_compat_enabled) {
-		goto orig_flow;
-	}
 
-	if (unlikely(ksu_execveat_hook)) {
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	}
-
-orig_flow:
-#endif
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
--- kernel/reboot.c
+++ kernel/reboot.c
@@ -311,9 +311,6 @@ extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
  * reboot doesn't sync: do that yourself before calling this.
  */
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
-#endif
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
@@ -321,14 +318,6 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU
-	ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
-	if (ret) {
-		goto orig_flow;
-	}
-	return ret;
-orig_flow:
-#endif
 
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
