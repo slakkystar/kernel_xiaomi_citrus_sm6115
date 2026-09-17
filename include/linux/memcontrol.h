895c9947cd2e73e5fbd3bff7914fe191cd8b95e9
SlakkyStar <slakkystar@gmail.com>
2026-09-17 10:02:57 +0500

    memcontrol: Add android_oem_data1 field to struct mem_cgroup
    Add android_oem_data1 field to struct mem_cgroup guarded by
    __GENKSYMS__ to prevent KABI breakages. This resolves compilation
    errors in vendor hybridswap driver accessing OEM data fields.
    
    Signed-off-by: SlakkyStar <slakkystar@gmail.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -316,6 +316,10 @@ struct mem_cgroup {
 	struct lru_gen_mm_list mm_list;
 #endif
 
+#ifndef __GENKSYMS__
+	u64 android_oem_data1;
+#endif
+
 	struct mem_cgroup_per_node *nodeinfo[0];
 	/* WARNING: nodeinfo must be the last member here */
 };
